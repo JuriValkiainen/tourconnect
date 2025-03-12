1ff672e0f2632039767f3024f2380c3cec61bd93
Evgeniia <evgenia.grigorieva@gmail.com>
2025-03-12 20:52:39 +0200

    create button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
-import NavigationBar  from './components/NavigationBar.jsx'
+import NavigationBar from './components/NavigationBar.jsx'
 import Header from './components/Header.jsx'
 import PageContent from './components/PageContent.jsx'
 import Footer from './components/Footer.jsx'
+import Button from './components/Button.jsx'
 import './App.css'
+
 
 function App() {
 
@@ -11,6 +13,7 @@ function App() {
       <div className='w3-light-grey'>
         <NavigationBar />
         <Header />
+        <Button>TEXT</Button>
         <PageContent />
         <Footer />
       </div>
---
+++ client/src/components/Button.jsx
@@ -0,0 +1,6 @@
+
+const Button = ({ children }) => {
+    return <button className="w3-bar-item w3-red">{children}</button>;
+  };
+// <Button>TEXT</Button>
+export default Button;
