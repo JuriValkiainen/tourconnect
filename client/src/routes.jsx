0d47d0592a576ddb7691c1615410fc00a44797c4
JuriValkiainen <jurivalkiainen@gmail.com>
2025-03-15 16:20:44 +0200

    Added components and pages: CitySelection ExcursionList, BookingForm, GuideDashboard, etc.

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,20 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
+//import ExcursionList from "./pages/ExcursionList";
+//import Booking from "./pages/BookingForm";
+//import Register from "./pages/Register";
+//import GuideRegister from "./pages/GuideRegister";
+//import GuideDashboard from "./pages/GuideDashboard";
 // import CitySelection from "./pages/CitySelection";
 // import ExcursionTypes from "./pages/ExcursionTypes";
-// import ExcursionList from "./pages/ExcursionList";
-// import Booking from "./pages/Booking";
-// import Register from "./pages/Register";
-// import GuideRegister from "./pages/GuideRegister";
-// import GuideDashboard from "./pages/GuideDashboard";
 
 const AppRoutes = () => {
     return (
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/city-selection" element={<CitySelection />} />
-        <Route path="/excursion-types" element={<ExcursionTypes />} />
         <Route path="/excursions" element={<ExcursionList />} />
+        <Route path="/excursion-types" element={<ExcursionTypes />} />
         <Route path="/booking" element={<Booking />} />
         <Route path="/register" element={<Register />} />
         <Route path="/guide-register" element={<GuideRegister />} />
