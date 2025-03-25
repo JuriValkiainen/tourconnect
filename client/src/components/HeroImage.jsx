26964e5e53ae3d50bf5bcb08e865ddc1f0c5f4e1
JuriValkiainen <jurivalkiainen@gmail.com>
2025-03-25 16:23:58 +0200

    Create guide registration form, connect it to server, add email support, add guide registration button, and make TourConnect link go to home page

---
+++ client/src/components/HeroImage.jsx
@@ -0,0 +1,11 @@
+const HeroImage = () => (
+    <div className="w3-display-container">
+        <img
+          src="https://www.w3schools.com/w3images/london2.jpg"
+          alt="London"
+          style={{ width: "100%"}}
+        />
+    </div>
+);
+
+export default HeroImage;
