a18817770df614deb311cb0c8105c6009e9b8d17
JuriValkiainen <jurivalkiainen@gmail.com>
2025-04-09 09:39:51 +0300

    Add route for guide login page

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -4,6 +4,7 @@ import Booking from "./pages/BookingForm";
 import ExcursionList from "./pages/ExcursionList";
 import Register from "./pages/Register";
 import GuideRegister from "./pages/GuideRegister";
+import GuideLogin from "./pages/GuideLogin";
 import GuideDashboard from "./pages/GuideDashboard";
 import ExcursionDetail from "./pages/ExcursionDetail";
 // import CitySelection from "./pages/CitySelection";
@@ -16,8 +17,9 @@ const AppRoutes = () => {
         <Route path="/booking" element={<Booking />} />
         <Route path="/excursions" element={<ExcursionList />} />
         <Route path="/excursions/:id" element={<ExcursionDetail />} />
-        <Route path="/guide-dashboard" element={<GuideDashboard />} />
         <Route path="/guide-register" element={<GuideRegister />} />
+        <Route path="/guide-login" element={<GuideLogin />} />
+        <Route path="/guide-dashboard" element={<GuideDashboard />} />
         <Route path="/register" element={<Register />} />
         {/* <Route path="/city-selection" element={<CitySelection />} />
         <Route path="/excursion-types" element={<ExcursionTypes />} /> */}
