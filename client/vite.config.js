98bf7a6eaf3f9e2fad527c3a5d0782e7a680b49d
JuriValkiainen <jurivalkiainen@gmail.com>
2025-04-10 21:19:01 +0300

    Configure Vite proxy for backend API

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/guides': 'http://localhost:5001',
+      '/api': 'http://localhost:5001',
+    },
+  },
 })
