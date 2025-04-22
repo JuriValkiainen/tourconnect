02856b6fef2f8d68fb8efbf9a73d955c324c37d4
AndreyGOIT <ae.borga@gmail.com>
2025-04-22 10:24:57 +0300

    some fix verifyEmail

--- client/src/pages/VerifyEmail.jsx
+++ client/src/pages/VerifyEmail.jsx
@@ -9,41 +9,41 @@ const VerifyEmail = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    console.log("URL с токеном:", window.location.href);
+    console.log("URL with token:", window.location.href);
   }, []);
 
   useEffect(() => {
     if (token) {
       const verifyToken = async () => {
         try {
-          console.log("Проверяем токен в ф-ии VerifyEmail:", token);
+          console.log("We check the token in the VerifyEmail function:", token);
           const response = await axios.get('http://localhost:5001/verify-email', {
             params: { token }
           });
       
           if (response.status === 200) {
-            setVerificationStatus('Электронная почта успешно подтверждена!');
+            setVerificationStatus('Email successfully verified!');
             setTimeout(() => {
               navigate('/profile');
             }, 3000);
           } else {
-            setVerificationStatus(`Ошибка подтверждения: ${response.data.error || 'Неверный токен'}`);
+            setVerificationStatus(`Assertion error: ${response.data.error || 'Invalid token'}`);
           }
         } catch (error) {
-          console.error('Ошибка при верификации email:', error);
-          setVerificationStatus('Произошла ошибка при верификации.');
+          console.error('Error verifying email:', error);
+          setVerificationStatus('An error occurred during verification.');
         }
       };
 
       verifyToken();
     } else {
-      setVerificationStatus('Неверная ссылка верификации.');
+      setVerificationStatus('Invalid verification link.');
     }
   }, [token, navigate]);
 
   return (
     <div className="w3-container w3-padding-64 w3-center">
-      <h1>Подтверждение электронной почты</h1>
+      <h1>Email confirmation</h1>
       <p>{verificationStatus}</p>
     </div>
   );
