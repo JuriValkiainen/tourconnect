751cd8e5d6881760226b089e349db330d0c3512d
AndreyGOIT <ae.borga@gmail.com>
2025-04-23 18:23:35 +0300

    add new pages Blog, Reviews, Support and Register + i18 for all

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,109 +1,140 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
 const Home = () => {
-
-const navigate = useNavigate();
-const handleImageClick = (tour) => {
-  navigate(`/excursions/${tour.tourID}`, { state: { date: new Date().toISOString().slice(0, 10) } });
-};
-
   const [picTour, setPicTour] = useState([]);
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+  
+  const handleImageClick = (tour) => {
+    navigate(`/excursions/${tour.tourID}`, {
+      state: { date: new Date().toISOString().slice(0, 10) },
+    });
+  };
+
   useEffect(() => {
-    axios.get("http://localhost:5001/randomtours")
-      .then(response => {
+    axios
+      .get("http://localhost:5001/randomtours")
+      .then((response) => {
         const tours = [...new Set(response.data)];
         setPicTour(tours);
       })
-      .catch(error => console.error("Error fetching pictures:", error));
+      .catch((error) => console.error("Error fetching pictures:", error));
   }, []);
-
 
   return (
     <div className="w3-content" style={{ maxWidth: "1100px" }}>
       {/* <!-- Good offers --> */}
       <div className="w3-container w3-margin-top">
-        <h3>Good Offers Right Now</h3>
-        <h5> Where will you go next? </h5>
+        <h3>{t("home_goodOffers_title1")}</h3>
+        <h5>{t("home_goodOffers_title2")}</h5>
       </div>
       <div className="w3-row-padding w3-text-white w3-large">
-      {picTour[0] && (
-    <div
-      className="w3-half w3-margin-bottom"
-      onClick={() => handleImageClick(picTour[0])}
-      style={{ cursor: "pointer" }}
-    >
-      <div className="img-wrapper tall-img"
-      style={{
-        height: "456px", 
-        overflow: "hidden", 
-        position: "relative"}}>
-        <img
-          src={picTour[0].picture}
-          alt={picTour[0].city}
-          style={{ width: "100%", height: "100%", objectFit: "cover", display: "block" }}
-        />
-        <span className="w3-display-bottomleft w3-padding">
-          {picTour[0].city}
-        </span>
+        {picTour[0] && (
+          <div
+            className="w3-half w3-margin-bottom"
+            onClick={() => handleImageClick(picTour[0])}
+            style={{ cursor: "pointer" }}
+          >
+            <div
+              className="img-wrapper tall-img"
+              style={{
+                height: "456px",
+                overflow: "hidden",
+                position: "relative",
+              }}
+            >
+              <img
+                src={picTour[0].picture}
+                alt={picTour[0].city}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  display: "block",
+                }}
+              />
+              <span className="w3-display-bottomleft w3-padding">
+                {picTour[0].city}
+              </span>
+            </div>
+          </div>
+        )}
+        <div className="w3-half">
+          <div className="w3-row-padding" style={{ margin: "0 -16px" }}>
+            {[1, 2].map(
+              (i) =>
+                picTour[i] && (
+                  <div
+                    key={picTour[i].tourID}
+                    className="w3-half w3-margin-bottom"
+                    onClick={() => handleImageClick(picTour[i])}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <div
+                      style={{
+                        height: "220px",
+                        overflow: "hidden",
+                        position: "relative",
+                      }}
+                    >
+                      <img
+                        src={picTour[i].picture}
+                        alt={picTour[i].city}
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                          display: "block",
+                        }}
+                      />
+                      <span className="w3-display-bottomleft w3-padding">
+                        {picTour[i].city}
+                      </span>
+                    </div>
+                  </div>
+                )
+            )}
+          </div>
+          <div className="w3-row-padding" style={{ margin: "0 -16px" }}>
+            {[3, 4].map(
+              (i) =>
+                picTour[i] && (
+                  <div
+                    key={picTour[i].tourID}
+                    className="w3-half w3-margin-bottom"
+                    onClick={() => handleImageClick(picTour[i])}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <div
+                      style={{
+                        height: "220px",
+                        overflow: "hidden",
+                        position: "relative",
+                      }}
+                    >
+                      <img
+                        src={picTour[i].picture}
+                        alt={picTour[i].city}
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                          display: "block",
+                        }}
+                      />
+                      <span className="w3-display-bottomleft w3-padding">
+                        {picTour[i].city}
+                      </span>
+                    </div>
+                  </div>
+                )
+            )}
+          </div>
+        </div>
       </div>
-    </div>
-  )}
-  <div className="w3-half">
-    <div className="w3-row-padding" style={{ margin: "0 -16px" }}>
-      {[1, 2].map(
-        (i) =>
-          picTour[i] && (
-            <div
-              key={picTour[i].tourID}
-              className="w3-half w3-margin-bottom"
-              onClick={() => handleImageClick(picTour[i])}
-              style={{ cursor: "pointer" }}
-            >
-              <div style={{ height: "220px", overflow: "hidden", position: "relative" }}>
-                <img
-                  src={picTour[i].picture}
-                  alt={picTour[i].city}
-                  style={{ width: "100%", height: "100%", objectFit: "cover", display: "block" }}
-                />
-                <span className="w3-display-bottomleft w3-padding">
-                  {picTour[i].city}
-                </span>
-              </div>
-            </div>
-          )
-      )}
-    </div>
-    <div className="w3-row-padding" style={{ margin: "0 -16px" }}>
-      {[3, 4].map(
-        (i) =>
-          picTour[i] && (
-            <div
-              key={picTour[i].tourID}
-              className="w3-half w3-margin-bottom"
-              onClick={() => handleImageClick(picTour[i])}
-              style={{ cursor: "pointer" }}
-            >
-              <div style={{ height: "220px", overflow: "hidden", position: "relative" }}>
-                <img
-                  src={picTour[i].picture}
-                  alt={picTour[i].city}
-                  style={{ width: "100%", height: "100%", objectFit: "cover", display: "block" }}
-               
-                />
-                <span className="w3-display-bottomleft w3-padding">
-                  {picTour[i].city}
-                </span>
-              </div>
-            </div>
-          )
-      )}
-    </div>
-  </div>
-</div>
-     
 
       {/* <!-- Explore Nature --> */}
       <div className="w3-container">
